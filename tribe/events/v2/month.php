072eb35a7a27de3aa7e27b640907ac143a4d49e5
langj123 <88846488+web-redegg-marketing@users.noreply.github.com>
2025-05-11 19:42:09 -0600

    fix for event page

--- tribe/events/v2/month.php
+++ tribe/events/v2/month.php
@@ -34,7 +34,7 @@ if ( empty( $disable_event_search ) ) {
 <div class="wp-block-ddrc-theme-blocks-header-intro header-intro-block">
 	<div class="block-wrapper">
 		<div class="block-content">
-			<h2 class="wp-block-heading header-title">Upcoming Events</h2>
+			<h2 class="wp-block-heading header-title middle-active"><middleline>Upcoming Events</middleline></h2>
 				<p>From fundraising galas to volunteer days, our events bring people together to celebrate and support the work we do. These gatherings not only raise vital funds but also foster connections among supporters and participants. Come and join us so you can see firsthand the impact of your generosity.</p>
 
 		</div>
